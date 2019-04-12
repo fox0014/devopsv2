b706ee5f9fecdb3afae0be6f67f8cad278db936a
fanfanfan <284130129@qq.com>
2019-04-12 17:29:07 +0800

    push

--- devopsV2/myhttp/urls.py
+++ devopsV2/myhttp/urls.py
@@ -1,8 +1,10 @@
 
 from django.urls import path
  
-from . import views
+#from . import views
+from .myviews import views
  
 urlpatterns = [
+    path('index2', views.index2, name='index2'),
     path('<str:service>', views.index, name='index'),
 ]
